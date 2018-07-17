23d07d22b65fffe8e6973c790bc3ad18149ceff2
GabrielTorres7 <gabieltorres2000@gmail.com>
2018-07-17 20:49:06 -0300

    Finalizando interfaces DAO

--- FarmazCore/src/br/cefetmg/farmaz/model/dao/EnderecoDAO.java
+++ FarmazCore/src/br/cefetmg/farmaz/model/dao/EnderecoDAO.java
@@ -5,10 +5,18 @@
  */
 package br.cefetmg.farmaz.model.dao;
 
+import br.cefetmg.farmaz.model.dominio.Endereco;
+import br.cefetmg.farmaz.model.exception.PersistenciaException;
+import java.util.List;
+
 /**
  *
  * @author Gabriel
  */
 public interface EnderecoDAO {
-    
+    public Long insert(Endereco endereco) throws PersistenciaException;
+    public boolean update(Endereco endereco) throws PersistenciaException;
+    public boolean remove(Long enderecoId) throws PersistenciaException;
+    public Endereco getEnderecoById(Long enderecoId) throws PersistenciaException;
+    public List<Endereco> getEnderecosByClienteId(Long clienteId) throws PersistenciaException;
 }
