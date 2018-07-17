080d7812aa8c0f8b425868cf5b61f46d1efa7356
GabrielTorres7 <gabieltorres2000@gmail.com>
2018-07-17 20:25:34 -0300

    Criando util bd e iniciando ClienteDAO

--- FarmazCore/src/br/cefetmg/farmaz/model/daoImpl/ClienteDAOImpl.java
+++ FarmazCore/src/br/cefetmg/farmaz/model/daoImpl/ClienteDAOImpl.java
@@ -8,7 +8,14 @@ package br.cefetmg.farmaz.model.daoImpl;
 import br.cefetmg.farmaz.model.dao.ClienteDAO;
 import br.cefetmg.farmaz.model.dominio.Cliente;
 import br.cefetmg.farmaz.model.exception.PersistenciaException;
+import br.cefetmg.farmaz.util.bd.ManterConexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,7 +36,41 @@ public class ClienteDAOImpl implements ClienteDAO{
     
     @Override
     public Long insert(Cliente cliente) throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (cliente == null){
+            throw new PersistenciaException("Domínio não pode ser nulo.");
+        }
+        
+        Long clienteId = null;
+        
+        try {
+            Connection connection = ManterConexao.getInstance().getConnection();
+            
+            String sql = "INSERT INTO cliente (nom_cliente, nom_email, txt_password, txt_documento, num_telefone) " +
+                         "    VALUES (?, ?, md5(?), ?, ?) ";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setString(2, cliente.getEmail());
+            pstmt.setString(3, cliente.getSenha());
+            pstmt.setString(4, cliente.getDocumentoIdentificacao());
+            pstmt.setString(5, cliente.getNumeroTelefone());
+            ResultSet rs = pstmt.executeQuery("SELECT LAST_INSERT_ID() FROM usuario");
+
+            if (rs.next()) {
+                clienteId = rs.getLong(1);
+                cliente.setId(clienteId);
+            }
+
+            rs.close();
+            pstmt.close();
+            connection.close();
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ClienteDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException(ex);
+        }
+        
+        return clienteId;
     }
 
     @Override
---
+++ FarmazCore/src/br/cefetmg/farmaz/util/bd/ConexaoMySQL.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.farmaz.util.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class ConexaoMySQL implements ConnectionFactory {
+
+    private final static String dbDriver = "com.mysql.jdbc.Driver";
+    private final static String dbURL = "jdbc:mysql://localhost:8080/farmaz";
+    private final static String usuario = "root";
+    private final static String senha = "";
+    
+    public ConexaoMySQL() {
+    }
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(dbDriver);
+        return DriverManager.getConnection(dbURL, usuario, senha);
+    }
+    
+}
