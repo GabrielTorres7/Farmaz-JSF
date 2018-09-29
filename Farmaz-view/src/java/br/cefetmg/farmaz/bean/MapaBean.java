d12a2751cf0ac85dc73d7afdbbf064940766657a
Arthur Codama <arthur_codama@hotmail.com>
2018-09-29 01:02:16 -0300

    Implementando mapa

--- Farmaz-view/src/java/br/cefetmg/farmaz/bean/MapaBean.java
+++ Farmaz-view/src/java/br/cefetmg/farmaz/bean/MapaBean.java
@@ -9,6 +9,66 @@ package br.cefetmg.farmaz.bean;
  *
  * @author Arthur
  */
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.primefaces.event.map.GeocodeEvent;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.GeocodeResult;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+ 
+@ManagedBean(name = "mapaBean")
+@RequestScoped
 public class MapaBean {
+    private String endereco = "Avenida Afonso Pena - Centro, Belo Horizonte - MG, Brasil";
+    private MapModel geoModel;
+    private String centerGeoMap = "41.850033,-87.6500523";
+    
+    @PostConstruct
+    public void init() {
+        geoModel = new DefaultMapModel();
+    }
+    
+    public void onGeocode(GeocodeEvent event) {
+        List<GeocodeResult> results = event.getResults();
+         
+        if (results != null && !results.isEmpty()) {
+            LatLng center = results.get(0).getLatLng();
+            centerGeoMap = center.getLat() + "," + center.getLng();
+             
+            for (int i = 0; i < results.size(); i++) {
+                GeocodeResult result = results.get(i);
+                geoModel.addOverlay(new Marker(result.getLatLng(), result.getAddress()));
+            }
+        }
+    }
+    
+    public MapModel getGeoModel() {
+        return geoModel;
+    }
+
+    public void setGeoModel(MapModel geoModel) {
+        this.geoModel = geoModel;
+    }
+
+    public String getCenterGeoMap() {
+        return centerGeoMap;
+    }
+
+    public void setCenterGeoMap(String centerGeoMap) {
+        this.centerGeoMap = centerGeoMap;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
     
 }
