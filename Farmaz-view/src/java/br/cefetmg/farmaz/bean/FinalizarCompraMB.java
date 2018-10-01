7d298399e4dd08a877193b2808e7aab965c3e360
Hiagozeroum <hiagozeroum@gmail.com>
2018-09-30 21:51:31 -0300

    Atualizando FinalizaCompra

--- Farmaz-view/src/java/br/cefetmg/farmaz/bean/FinalizarCompraMB.java
+++ Farmaz-view/src/java/br/cefetmg/farmaz/bean/FinalizarCompraMB.java
@@ -5,10 +5,24 @@
  */
 package br.cefetmg.farmaz.bean;
 
+import br.cefetmg.farmaz.model.dominio.Disponibilidade;
+import br.cefetmg.farmaz.model.dominio.Farmacia;
+import br.cefetmg.farmaz.model.dominio.Produto;
+import br.cefetmg.farmaz.model.exception.PersistenciaException;
+import br.cefetmg.farmaz.proxy.ManterDisponibilidadeProxy;
+import br.cefetmg.farmaz.proxy.ManterFarmaciaProxy;
+import br.cefetmg.farmaz.proxy.ManterProdutoProxy;
+import br.cefetmg.farmaz.util.session.SessionContext;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -19,12 +33,29 @@ import javax.faces.bean.ViewScoped;
 public class FinalizarCompraMB {
 
     /**
-     * Creates a new instance of FinalizarCompraMB
+     * @return the visibilidade
      */
-    public FinalizarCompraMB() {
-    }
+
+
+    ManterDisponibilidadeProxy manterDisponibilidade =  new ManterDisponibilidadeProxy();
+    private Disponibilidade item = new Disponibilidade();
+    List<Disponibilidade> carrinho;
+    private String troco ;
+    FazerPedidoMB pedido =  new FazerPedidoMB();
+
     private boolean visibilidade = false;
+
+    public List<Disponibilidade> getCarrinho(){
+        carrinho = (List<Disponibilidade>) SessionContext.getInstance().getAttribute("MeuCarrinho");
+        return carrinho;
+    }
     
+    public FinalizarCompraMB() throws SocketException, UnknownHostException {
+    }
+   
+    public boolean isVisibilidade() {
+        return visibilidade;
+    }
     public void show(){
         visibilidade=true;
     }
@@ -33,12 +64,41 @@ public class FinalizarCompraMB {
         visibilidade=false;
     }
 
-    public boolean isVisibilidade() {
-        return visibilidade;
-    }
-
     public void setVisibilidade(boolean visibilidade) {
         this.visibilidade = visibilidade;
     }
+
+    public void setTroco(String troco) {
+        this.troco = troco;
+    }
+
+    public String getTroco() {
+        return troco;
+    }
+    public void enviar() throws IOException, PersistenciaException{
+        SessionContext.getInstance().setAttribute("troco", getTroco());
+        Double total = 0.0;
+                        Produto produto;
+                        Farmacia farmacia;
+                        String nomeProduto;
+                        String nomeFarmacia;
+                        ManterProdutoProxy manterProduto = new ManterProdutoProxy();
+                        ManterFarmaciaProxy manterFarmacia = new ManterFarmaciaProxy();
+                        List<Disponibilidade> carrinho =(List<Disponibilidade>) SessionContext.getInstance().getAttribute("MeuCarrinho");
+                        for (Disponibilidade item: carrinho) {
+                            produto = manterProduto.getProdutoById(item.getProdutoSeq());
+                            farmacia = manterFarmacia.getFarmaciaById(item.getFarmaciaCadastro());
+                            nomeProduto = produto.getNome();
+                            nomeFarmacia = farmacia.getNome();
+                            total += item.getPreco();
+                            SessionContext.getInstance().setAttribute("Total",total);
+                        }
+        pedido.Executa();
+       
+    }
+            
+
+    
+    
     
 }
